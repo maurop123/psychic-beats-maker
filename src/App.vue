b59d608975f518f4a001551a06b57c69702a4c18
Mauro P <maurop123@gmail.com>
2023-11-02 13:42:11 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+/* import HelloWorld from './components/HelloWorld.vue' */
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-full">
+    <h1>Hello wooorld</h1>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    color: blue;
+
+    &:hover {
+      color: red;
+    }
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1 class="text-red-600">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a Vue 3 project
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+</style>
